018b9e64fd16e6c689a31ac1926ce498338792d5
NidanStha <nidanshrestha@gmail.com>
2020-02-12 18:26:42 +0545

    completed side nav, field with includes

---
+++ master.php
@@ -0,0 +1,11 @@
+<html>
+<head>
+  <?php include "header.php"; ?>
+</head>
+<body>
+  <?php
+    include "nav.php";
+    include "field.php";
+  ?>
+</body>
+</html>
---
+++ nav.php
@@ -0,0 +1,11 @@
+<nav class="side-nav">
+  <h2 class="main-title">Employee</h2>
+  <h2 class="main-title">Management</h2>
+  <h2 class="main-title">System</h2>
+  <ul class="nav-opr">
+    <li><a href="">Employee Status</a></li>
+    <li><a href="">Add Employee</a></li>
+    <li><a href="">Update Employee</a></li>
+    <li><a href="">Delete Employee</a></li>
+  </ul>
+</nav>
